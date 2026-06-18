edec54cfb3318f3f8345f94b305203a0085ee106
kienanh210100abc <duckien210100@gmail.com>
2026-06-18 12:09:11 +0700

    menu header

--- src/types/interfaces/navigation.ts
+++ src/types/interfaces/navigation.ts
@@ -2,12 +2,21 @@ import type { Segment } from "@/types/enums/segment";
 
 export interface NavigationChildItem {
   key: string;
-  href: string;
+  href?: string;
+  category?: string;
+  label?: string;
+  items?: Array<{
+    key: string;
+    label: string;
+    href?: string;
+  }>;
 }
 
 export interface NavigationItem {
   key: string;
   href?: string;
+  label?: string;
+  layout?: "grid" | "list";
   children?: NavigationChildItem[];
 }
 
