59e2dd77f9af5c1a03a910cef1edf2115777a78a
VSII-hoangtt <tranhoang07042001@gmail.com>
2026-07-14 13:42:56 +0700

    add path img

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const createNextIntlPlugin = require("next-intl/plugin");
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+            },
+        ],
+    },
+};
 
 module.exports = withNextIntl(nextConfig);
